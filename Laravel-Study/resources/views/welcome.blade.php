82d92e0e62c4e7b94a9137b0011b01a7fbf03202
hyeminnoh <shrmffkfk@naver.com>
2020-07-06 14:35:18 +0900

    [Update] blade engine 활용

--- Laravel-Study/resources/views/welcome.blade.php
+++ Laravel-Study/resources/views/welcome.blade.php
@@ -1,17 +1,5 @@
 @extends('layouts/master')
 
-@section('style')
-    <style>
-        body {background: darkgray; color: white;}
-    </style>
-@endsection
-
 @section('content')
-    <p>저는 자식 뷰의 'content section' 입니다.</p>
-@endsection
-
-@section('script')
-    <script>
-        alert("저는 자식 뷰의 'script section'입니다.");
-    </script>
+    @include('partials.footer')
 @endsection
