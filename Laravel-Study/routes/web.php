c9e1b9fed3737217a915afb2eb67eab9243e4bff
hyeminnoh <shrmffkfk@naver.com>
2020-07-07 13:45:22 +0900

    [Add] 사용자 인증 기능 활용

--- Laravel-Study/routes/web.php
+++ Laravel-Study/routes/web.php
@@ -14,4 +14,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'WelcomeController@index');
+
+// 자체 인증
+Route::get('auth/login', function() {
+    $credentials = [
+        'email' => 'john@example.com',
+        'password' => 'password'
+    ];
+    if(! auth()->attempt($credentials)){
+        return '로그인 정보가 정확하지 않습니다.';
+    }
+    return redirect('protected');
+});
+Route::get('protected', ['middleware' => 'auth', function(){
+    dump(session()->all());
+    return '어서오세요' . auth()->user()->name;
+}]);
+Route::get('auth/logout', function(){
+    auth()->logout();
+    return '또 봐요~';
+});
+
 Route::resource('articles', 'ArticlesController');
+
+// 내장 인증
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
