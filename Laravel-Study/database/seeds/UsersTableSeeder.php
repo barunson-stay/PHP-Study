56b5b0dcd59a8031cf27c6fed19a7c0225bb68fa
hyeminnoh <shrmffkfk@naver.com>
2020-07-07 15:54:14 +0900

    [Add] Database Seeding

---
+++ Laravel-Study/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*
+        App\User::create([
+            'name' => sprintf('%s %s', Str::random(3), Str::random(4)),
+            'email' => Str::random(10) . '@example.com',
+            'password' => bcrypt('password'),
+        ]);
+        */
+        factory(App\User::class, 5)->create();
+    }
+}
