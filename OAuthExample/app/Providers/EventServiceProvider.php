9b3a2dc59d4e823f6286fb7606df0a2896a9eb39
hyeminnoh <shrmffkfk@naver.com>
2020-07-13 17:39:45 +0900

    [Update] Add UserEventListener

--- OAuthExample/app/Providers/EventServiceProvider.php
+++ OAuthExample/app/Providers/EventServiceProvider.php
@@ -20,6 +20,10 @@ class EventServiceProvider extends ServiceProvider
         ],
     ];
 
+    protected $subscribe = [
+        \App\Listeners\UsersEventListener::class,
+    ];
+
     /**
      * Register any events for your application.
      *
