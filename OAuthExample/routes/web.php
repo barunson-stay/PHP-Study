9c7dbff7246cfa533eab7f68cd819eb6ef26990a
hyeminnoh <shrmffkfk@naver.com>
2020-07-13 17:34:29 +0900

    [Update] 인증 로직 직접 구현

--- OAuthExample/routes/web.php
+++ OAuthExample/routes/web.php
@@ -1,26 +1,65 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+Route::get('auth/login', [
+    'as' => 'sessions.create',
+    'uses' => 'SessionsController@create',
+]);
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [
+    'as' => 'root',
+    'uses' => 'WelcomeController@index',
+]);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', [
+    'as' => 'home',
+    'uses' => 'HomeController@index',
+]);
 
-Auth::routes();
+/* 사용자 등록 */
+Route::get('auth/register', [
+    'as' => 'users.create',
+    'uses' => 'UsersController@create',
+]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::post('auth/register', [
+    'as' => 'users.store',
+    'uses' => 'UsersController@store',
+]);
 
-Auth::routes();
+Route::get('auth/confirm/{code}', [
+    'as' => 'users.confirm',
+    'uses' => 'UsersController@confirm',
+])->where('code', '[\pL-\pN]{60}');
 
-Route::get('/home', 'HomeController@index')->name('home');
+/* 사용자 인증 */
+
+Route::post('auth/login', [
+    'as' => 'sessions.store',
+    'uses' => 'SessionsController@store',
+]);
+
+Route::get('auth/logout', [
+    'as' => 'sessions.destroy',
+    'uses' => 'SessionsController@destroy',
+]);
+
+/* 비밀번호 초기화 */
+Route::get('auth/remind', [
+    'as' => 'remind.create',
+    'uses' => 'PasswordsController@getRemind',
+]);
+
+Route::post('auth/remind', [
+    'as' => 'remind.store',
+    'uses' => 'PasswordsController@postRemind',
+]);
+
+Route::get('auth/reset/{token}', [
+    'as' => 'reset.create',
+    'uses' => 'PasswordsController@getReset',
+])->where('token', '[\pL-\pN]{64}');
+
+Route::post('auth/reset', [
+    'as' => 'reset.store',
+    'uses' => 'PasswordsController@postReset',
+]);
