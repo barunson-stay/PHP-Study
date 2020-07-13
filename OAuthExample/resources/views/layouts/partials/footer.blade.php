83ab9e4ea72c68cd78539e715068ddf8619d2de6
hyeminnoh <shrmffkfk@naver.com>
2020-07-13 10:45:47 +0900

    [Update] UI 수정

---
+++ OAuthExample/resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="container footer__master">
+
+    <div>
+        &copy; {{ date('Y') }}
+        <a href="{{ config('project.url') }}">
+            {{ config('app.name') }}
+        </a>
+    </div>
+</footer>
